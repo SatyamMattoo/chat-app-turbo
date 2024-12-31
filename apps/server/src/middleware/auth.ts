a48b0895471ee4bbc542146e5465985ce7ad2ed9
Satyam Mattoo <satyammattoo2003@gmail.com>
2024-12-31 09:46:15 +0000

    production: fixing cookie issues

--- apps/server/src/middleware/auth.ts
+++ apps/server/src/middleware/auth.ts
@@ -4,18 +4,16 @@ import { decode } from "@auth/core/jwt";
 import ErrorHandler from "./errorHandler.js";
 import { Socket } from "socket.io";
 
+const cookieName = "authjs.session-token";
+
 export const authMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  const cookieName =
-    process.env.ENVIRONMENT === "production"
-      ? "__Secure-authjs.session-token"
-      : "authjs.session-token";
+  console.log("auth middleware :", req);
   const token = req.cookies[cookieName];
   console.log(cookieName, " : ", token);
-  console.log("auth middleware :", req.cookies);
   if (!token) {
     return next(new ErrorHandler("Please login to access this resource!", 401));
   }
@@ -44,11 +42,6 @@ export const socketMiddleware = async (
   socket: Socket,
   next: (err?: Error) => void,
 ) => {
-  const cookieName =
-    process.env.ENVIRONMENT === "production"
-      ? "__Secure-authjs.session-token"
-      : "authjs.session-token";
-
   const cookies = socket.handshake.headers.cookie;
   console.log(socket.handshake.headers);
   const token = cookies
